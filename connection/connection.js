10f2977fb0894614138189170a0df87c5f038a27
shivkant patel <shivkantp159@gmail.com>
2025-08-29 22:03:26 +0530

    correctuon

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mysql2 = require('mysql2');
 
 let connecton =  mysql2.createConnection({
-    host:'your-db-host.render.com',
+    host:'localhost',
     user: 'root',
     password:'Shivkant77@',
     database:'school_management_system'
