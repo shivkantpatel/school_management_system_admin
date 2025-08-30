0d116ed086dd8476b67ac7e1eaecf01e3ac69b65
shivkant patel <shivkantp159@gmail.com>
2025-08-30 09:19:00 +0530

    commit

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const { body, validationResult } = require('express-validator');
  const dt= require ('dotenv')
 dt.config()
 const cors = require('cors')
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors())
 
 app.set('view engine','ejs');
