e0313f2fccd84e3164d823a5a25c9a22f3c3b01d
shivkant patel <shivkantp159@gmail.com>
2025-09-04 12:51:52 +0530

    added

--- connection/connection.js
+++ connection/connection.js
@@ -1,29 +1,15 @@
 
-// const mysql = require('mysql2');
-const { Pool } = require('pg');
+const mysql = require('mysql2');
 
-let connection = new Pool({
+let connection = mysql.createConnection({
 
-    host: 'dpg-d2rtv9jipnbc73e0gil0-a',           // ✅ Correct Railway host
-    user: 'school_management_system',
-    port: 5432,                            // ✅ Correct Railway port
-    password: '5LhPW2YYdTQ90ActdLxcjB6xIeyRm8Bo',
-    database: 'school_management_system_vqrr'
+    host: 'metro.proxy.rlwy.net',           // ✅ Correct Railway host
+    user: 'root',
+    port: 26525,                            // ✅ Correct Railway port
+    password: 'sLNcmhAoURrTLIVBfvwORoezBHfJaaWe',
+    database: 'school_management_system'
 });
 
- module.exports = connection
 
-// const mysql = require('mysql2');
-
-// let connection = mysql.createConnection({
-
-//     host: 'metro.proxy.rlwy.net',           // ✅ Correct Railway host
-//     user: 'root',
-//     port: 26525,                            // ✅ Correct Railway port
-//     password: 'sLNcmhAoURrTLIVBfvwORoezBHfJaaWe',
-//     database: 'school_management_system'
-// });
-
-
-// module.exports = connection
+module.exports = connection
 
